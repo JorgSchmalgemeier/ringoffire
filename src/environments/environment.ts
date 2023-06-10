d3ced8c69dd9b83c17e0b68308be5bf2a2ccf658
Jörg Schmalgemeier <j.schmalgemeier@googlemail.com>
2023-06-10 18:31:08 +0200

    made the project responsiv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   firebase: {
-    projectId: 'ring-of-fire-bffba',
-    appId: '1:123828184490:web:8af0da0a219b4276a537d3',
-    storageBucket: 'ring-of-fire-bffba.appspot.com',
     apiKey: 'AIzaSyCP6_IA6gAh0PctyvG0QEdfRkW3scnJhWc',
     authDomain: 'ring-of-fire-bffba.firebaseapp.com',
+    projectId: 'ring-of-fire-bffba',
+    storageBucket: 'ring-of-fire-bffba.appspot.com',
     messagingSenderId: '123828184490',
+    appId: '1:123828184490:web:8af0da0a219b4276a537d3',
   },
 };
